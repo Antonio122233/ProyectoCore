b02a2eb0cc514f9a6f0d36fcc2fa53d3b87c1936
Antonio David Artola Saavedra <aartola@fama.com.ni>
2026-06-06 14:45:29 -0600

    Primera versión del proyecto con base de datos y arquitectura limpia

---
+++ Persistencia/DependencyInjection.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infraestructura
+{
+    internal class DependencyInjection
+    {
+    }
+}
---
+++ Persistencia/Persistencia/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infraestructura.Persistencia
+{
+    internal class AppDbContext
+    {
+    }
+}
