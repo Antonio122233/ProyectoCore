136690662f1b3d2037ffcee6a836405c90afb95c
Antonio David Artola Saavedra <aartola@fama.com.ni>
2026-06-11 10:43:00 -0600

    se terminó el MapMarcaEndpoints  con los metodos crud.

---
+++ Aplicacion/Common/ApiResponse.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aplicacion.Common
+{
+    public class ApiResponse<T>
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public T Data { get; set; }
+
+
+        public ApiResponse(bool success, string message, T data)
+        {
+            Success = success;
+            Message = message;
+            Data = data;
+        }
+
+        //en caso de fallo
+        public static ApiResponse <object> Fail (string message)
+        {
+            return new ApiResponse<object>(false, message, null);
+        }
+
+        //OK
+        public static ApiResponse<T> Ok(T data, string message)
+        {
+            return new ApiResponse<T>(true, message, data);
+        }
+
+    }
+}
--- WebApi/Endpoints/MarcaEndpoints.cs
+++ WebApi/Endpoints/MarcaEndpoints.cs
@@ -1,22 +1,75 @@
-﻿using Aplicacion.Interfaces;
+﻿using Aplicacion.Common;
+using Aplicacion.DTOs;
+using Aplicacion.Interfaces;
 
 namespace WebApi.Endpoints
 {
     public static class MarcaEndpoints
     {
-        public static WebApplication MapMarcaEndpoints (this WebApplication app)
+        public static WebApplication MapMarcaEndpoints(this WebApplication app)
         {
-            var group = app.MapGroup("/api/marca");
+            var group = app.MapGroup("/api/marca")
+                .WithTags("Marca"); ;
 
             //GET  ALL : api/marca
             group.MapGet("/", async (IMarcaService service)
                 =>
             {
-                var marcas = await service.GetAllAsync();
-                return Results.Ok(marcas);
+                var marcas = await service.GetAllAsync();               
+                return Results.Ok(ApiResponse<IEnumerable<MarcaDto>>
+                    .Ok(marcas, "Listado de Marcas"));
             });
 
 
+            // GET BY ID
+            group.MapGet("/{id:int}", async (int id, IMarcaService service) =>
+            {
+                var marca = await service.GetByIdAsync(id);
+
+                if (marca == null)
+                    return Results.NotFound(ApiResponse<object>.Fail("Marca no encontrada"));
+
+                var response = new ApiResponse<MarcaDto>(
+                                    true,
+                                    "Marca encontrada",
+                                    marca
+                                );
+                return Results.Ok(ApiResponse<MarcaDto>
+                .Ok(marca, "Marca encontrada"));
+            });
+
+
+            //  POST (Crear)
+            group.MapPost("/", async (MarcaCreateDto dto, IMarcaService service) =>
+            {
+                var nueva = await service.CreateAsync(dto);               
+                return Results.Created($"/api/marca/{nueva.Id}", ApiResponse<MarcaDto>.Ok(nueva, "Marca creada correctamente"));
+            });
+
+
+            // PUT (Actualizar)
+            group.MapPut("/{id:int}", async (int id, MarcaUpdateDto dto, IMarcaService service) =>
+            {
+                var actualizado = await service.UpdateAsync(id, dto);
+
+                if (!actualizado)
+                    return Results.NotFound(ApiResponse<object>.Fail("Marca no encontrada"));
+
+                return Results.NoContent(); // regresa un 204
+            });
+
+
+            //  DELETE (Eliminar)
+            group.MapDelete("/{id:int}", async (int id, IMarcaService service) =>
+            {
+                var eliminado = await service.DeleteAsync(id);
+
+                if (!eliminado)
+
+                    return Results.NotFound(ApiResponse<object>.Fail("Marca no encontrada"));
+
+                return Results.NoContent(); //regresa un 204
+            });
 
             return app;
         }
