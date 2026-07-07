ea264e93d353482482d3da38127f48f20166d684
Antonio Artola <adas199278@hotmail.com>
2026-07-07 13:17:52 -0600

    se crearon los dto para producto y sus operaciones crud

--- Aplicacion/DTOs/Producto/ProductoCreateDto.cs
+++ Aplicacion/DTOs/Producto/ProductoCreateDto.cs
@@ -6,19 +6,19 @@ namespace Aplicacion.DTOs.Producto
 {
     public class ProductoCreateDto
     {
-        public int? IdCategoria { get; set; }         
-        public int IdMarca { get; set; }              
-        public string? CodigoProducto { get; set; }   
-        public string Nombre { get; set; } = null!;   
-        public string? Descripcion { get; set; }      
-        public string? Color { get; set; }            
-        public string? Talla { get; set; }            
-        public string? Material { get; set; }         
-        public decimal Costo { get; set; }            
-        public decimal Precio { get; set; }           
-        public int ExistenciaActual { get; set; }     
-        public int ExistenciaMinima { get; set; }     
-        public DateOnly FechaRegistro { get; set; }                         
+        public int? IdCategoria { get; set; }
+        public int IdMarca { get; set; }
+        public string? CodigoProducto { get; set; }
+        public string Nombre { get; set; } = null!;
+        public string? Descripcion { get; set; }
+        public string? Color { get; set; }
+        public string? Talla { get; set; }
+        public string? Material { get; set; }
+        public decimal Costo { get; set; }
+        public decimal Precio { get; set; }
+        public int ExistenciaActual { get; set; }
+        public int ExistenciaMinima { get; set; }
+        public DateOnly FechaRegistro { get; set; } = DateOnly.FromDateTime(DateTime.Now);
 
     }
 }
---
+++ Aplicacion/DTOs/Producto/ProductoDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aplicacion.DTOs.Producto
+{
+    public class ProductoDto
+    {
+        public int IdProducto { get; set; }          
+        public int? IdCategoria { get; set; }        
+        public int IdMarca { get; set; }             
+        public string? CodigoProducto { get; set; }  
+        public string Nombre { get; set; } = null!;  
+        public string? Descripcion { get; set; }     
+        public string? Color { get; set; }           
+        public string? Talla { get; set; }           
+        public string? Material { get; set; }        
+        public decimal Costo { get; set; }           
+        public decimal Precio { get; set; }          
+        public int ExistenciaActual { get; set; }    
+        public int ExistenciaMinima { get; set; }    
+        public DateOnly FechaRegistro { get; set; }  
+        public bool EstadoRegistro { get; set; }     
+        public DateTime? FechaBaja { get; set; }     
+    }
+}
